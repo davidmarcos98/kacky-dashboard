ebe8ee0199912327f272983477bd463e5ba3455a
David Marcos <davidmarcosg98@gmail.com>
2024-10-24 19:45:07 +0200

    fix for single fin

--- components/dashboard.tsx
+++ components/dashboard.tsx
@@ -123,7 +123,7 @@ const Dashboard = ({maps, user, all=false}: {maps: any, user?: string, all?: boo
             )}
             {!editMode && (
                 /* <div className="flex flex-wrap inline gap-4 justify-center pt-6"> */
-                <div className={`grid ${all ? "sm:grid-cols-2 md:grid-cols-3 2xl:grid-cols-5" : (isMobile ? "grid-cols-1" : (maps.length % 2 == 0 ? "grid-cols-2" : "grid-cols-3"))} gap-5 justify-center pt-6 px-5 pb-20`}>
+                <div className={`grid ${all ? "sm:grid-cols-2 md:grid-cols-3 2xl:grid-cols-5" : (isMobile ? "grid-cols-1" : (maps.length == 1 ? "grid-cols-1" : maps.length % 2 == 0 ? "grid-cols-2" : "grid-cols-3"))} gap-5 justify-center pt-6 px-5 pb-20`}>
                     {filteredMaps?.map((map: any) => (
                         <MapCard mapPage={user ? false : true} allMaps={all} key={map.map ? map.map.name : map.name} map={map.map ? map.map : map} clip={map.clip} user={user || ''}/>
                     ))}
