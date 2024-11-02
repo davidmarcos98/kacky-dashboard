1418825f549b656c318e0eb83ed5b285de65783c
David Marcos <davidmarcosg98@gmail.com>
2024-11-02 10:47:33 +0100

    map images

--- components/mapView.tsx
+++ components/mapView.tsx
@@ -66,7 +66,7 @@ const MapView = ({map, clips, mapPage=false}: {map: Map, clips: Clip[], mapPage:
           </CardHeader>
           <Image
             className="object-fit mapImage"
-            src={`/images/noimagae.jpg`}
+            src={`/images/${map.name}.jpg`}
             sizes="100,100"
             isZoomed
           />
