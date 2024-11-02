10cb64e0a4ba569ec4a0591b8f5b166bbd81a650
David Marcos <davidmarcosg98@gmail.com>
2024-11-02 13:48:39 +0100

    fix

--- app/api/twitch/lastFinish/[user]/route.ts
+++ app/api/twitch/lastFinish/[user]/route.ts
@@ -26,7 +26,7 @@ export async function GET(req: NextRequest, { params }: { params: Promise<{ user
                     }
                 },
                 limit: 1,
-                orderBy: [desc(finishesTable.date)],
+                orderBy: [desc(finishesTable.id)],
             }
         }
     });
