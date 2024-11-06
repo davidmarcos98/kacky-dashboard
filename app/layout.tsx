bd722264c426d8ca5b6665dd8b2e2941300a76ff
David Marcos <davidmarcosg98@gmail.com>
2024-11-07 00:54:24 +0100

    add intax

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,7 @@ export default async function Document({ children }: { children: React.ReactNode
     {username: 'granady', twitch: 'granadyy'},
     {username: 'greep', twitch: 'greepthesheep'},
     {username: 'ingentea', twitch: 'ingentea'},
+    {username: 'intax', twitch: 'intaxtm'},
     {username: 'jnic', twitch: 'jnic'},
     {username: 'jxliano', twitch: 'jxliano'},
     {username: 'Lars_tm', twitch: 'lars_tm'},
