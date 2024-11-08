53482f24fa54e24dd6484f09e15ce48606440a14
David Marcos <davidmarcosg98@gmail.com>
2024-11-08 23:45:59 +0100

    fix

--- next.config.js
+++ next.config.js
@@ -3,12 +3,12 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: '/api/twitch/clip/:path/:path',
-        destination: '/api/twitch/clip',
+        source: '/api/twitch/clip/:path(\\d{1,})',
+        destination: '/api/twitch/clip/:path',
       },
       {
-        source: '/api/twitch/clip/:path',
-        destination: '/api/twitch/clip/:path',
+        source: '/api/twitch/clip/:path*',
+        destination: '/api/twitch/clip',
       },
     ]
   },
