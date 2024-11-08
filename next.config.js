3962bb141e5bb52d241059649a24ae246a867aaa
David Marcos <davidmarcosg98@gmail.com>
2024-11-08 23:40:19 +0100

    fix wirtual annoying chatters

--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/twitch/clip/:path/:path',
+        destination: '/api/twitch/clip',
+      },
+      {
+        source: '/api/twitch/clip/:path',
+        destination: '/api/twitch/clip/:path',
+      },
+    ]
+  },
   reactStrictMode: false,
 }
 
