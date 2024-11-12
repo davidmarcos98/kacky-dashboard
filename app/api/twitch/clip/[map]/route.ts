94b538658b5c3f7e1331d25d876d62909445430e
David Marcos <davidmarcosg98@gmail.com>
2024-11-12 01:42:22 +0100

    add fosa user for clip

--- app/api/twitch/clip/[map]/route.ts
+++ app/api/twitch/clip/[map]/route.ts
@@ -1,16 +1,25 @@
 import { NextResponse, NextRequest } from 'next/server';
 
 import 'dotenv/config';
-import { eq } from "drizzle-orm";
-import { mapsTable } from "@/db/schema";
+import { eq, and } from "drizzle-orm";
+import { finishesTable, mapsTable, usersTable } from "@/db/schema";
 import { db } from "@/db/client";
 
 export async function GET(req: NextRequest, { params }: { params: Promise<{ map: string }> }){
     /* 
         /addcom !clip $(customapi https://kacky.socr.am/api/twitch/clip/[MAP])
     */
+    const headers = req.headers;
+    let fossaUser = headers.get('x-fossabot-channeldisplayname')
+    let map;
+    let user;
 
-    const map = await db.query.mapsTable.findFirst({
+    if (fossaUser) {
+        user = await db.query.usersTable.findFirst({
+            where: eq(usersTable.twitch, fossaUser.toLowerCase()),
+        });
+    }
+    map = await db.query.mapsTable.findFirst({
         where: eq(mapsTable.name, (await params).map as string),
         with: {
             finishes: {
@@ -22,6 +31,12 @@ export async function GET(req: NextRequest, { params }: { params: Promise<{ map:
             }
         }
     });
+    if (map && user) {
+        const finish = await db.query.finishesTable.findFirst({
+            where: and(eq(finishesTable.mapId, map.id), eq(finishesTable.userId, user.id)),
+        })
+        return new NextResponse(`Clip for #${(await params).map}: ${finish?.clip}`);
+    }
 
     if (!map || !(map.finishes.length > 0)) {
         return new NextResponse(`No clip found for map ${(await params).map}`);
