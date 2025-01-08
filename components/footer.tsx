32eca38e9c97caf88f7a73d5320e64ed0bdb3c66
David Marcos <davidmarcosg98@gmail.com>
2025-01-08 02:46:54 +0100

    openplanet uwu

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,10 +5,8 @@ export default function Footer() {
     const pathname = usePathname();
 
     return (
-        <footer className={`flex fixed bottom-0 z-40 h-10 w-full bg-black ${pathname === "/leaderboard" ? 'justify-between' : 'justify-end'} items-center pl-4 pr-4`} style={{ bottom: 0}}>
-            {pathname === "/leaderboard" ? (
-                <span>Data from <a href='https://kacky.gg' className="underline" target="_blank">kacky.gg</a></span>
-            ) : (<></>)}
+        <footer className={`flex fixed bottom-0 z-40 h-10 w-full bg-black justify-between items-center pl-4 pr-4`} style={{ bottom: 0}}>
+            <span>Made possible by Openplanet</span>
             <span>Made by @socramdavid</span>
         </footer>
   );
